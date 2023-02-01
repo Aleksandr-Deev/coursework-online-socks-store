370d40e937fe19fc1312746897038df4b1414eed
Aleksandr Deev <geshtait@mail.ru>
2023-02-01 21:54:40 +0500

    Первый коммит

---
+++ src/main/java/com/example/courseworkonlinesocksstore/CourseworkOnlineSocksStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseworkonlinesocksstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkOnlineSocksStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseworkOnlineSocksStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/courseworkonlinesocksstore/CourseworkOnlineSocksStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseworkonlinesocksstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkOnlineSocksStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
